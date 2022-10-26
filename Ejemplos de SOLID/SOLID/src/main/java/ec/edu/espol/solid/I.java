453ca1e2b13f87fe482ce0d1fe66d10d74729b46
BryanEstrada003 <mbestrad@espol.edu.ec>
2022-10-26 16:22:07 -0500

    Ejemplo de Interface Segregation Principle

--- Ejemplos de SOLID/SOLID/src/main/java/ec/edu/espol/solid/I.java
+++ Ejemplos de SOLID/SOLID/src/main/java/ec/edu/espol/solid/I.java
@@ -11,4 +11,51 @@ package ec.edu.espol.solid;
  */
 public class I {
     
+// se realiza un sistema educativo donde al iniciar sesion se muestra la informacion de los usuarios
+    
+    /**
+     * CODIGO QUE VIOLA EL PRINCIPIO
+    class SistemaAcademico{
+        void mostrarInformacion(){
+            //codigo que muestra la informacion al iniciar sesion
+        };
+    };
+    
+    class Estudiante extends SistemaAcademico{
+        @Override
+        void mostrarInformacion(){
+            //codigo que muestra la informacion al iniciar sesion
+        };
+    };
+    class Maestro extends SistemaAcademico{
+        @Override
+        void mostrarInformacion(){
+            //codigo que muestra la informacion al iniciar sesion
+        };
+    };
+    */
+    
+    interface SistemaAcademicoEstudiantil{
+        //...
+        void mostrarInformacionEstudiante();
+        //...
+    };
+    interface SistemaAcademicoProfesor{
+        //...
+        void mostrarInformacionProfesor();
+        //...
+    };
+    
+    class Estudiante implements SistemaAcademicoEstudiantil{
+        //...
+        @Override
+        public void mostrarInformacionEstudiante(){};
+        //...
+    };
+    class Maestro implements SistemaAcademicoProfesor{
+        //...
+        @Override
+        public void mostrarInformacionProfesor(){};
+        //...
+    };
 }
