7a439cfba4009f7e05c503a8cc30cd3bc61a7f82
noeliapasaca <91957504+noeliapasaca@users.noreply.github.com>
2022-10-27 12:54:11 -0500

    Implementacion Ejemplo Dependency Inversion Principle

--- Ejemplos de SOLID/SOLID/src/main/java/ec/edu/espol/solid/D.java
+++ Ejemplos de SOLID/SOLID/src/main/java/ec/edu/espol/solid/D.java
@@ -1,9 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.espol.solid;
+
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  *
@@ -11,4 +15,74 @@ package ec.edu.espol.solid;
  */
 public class D {
     
+    public interface CustomerDao {
+
+        Optional<Customer> findById(int id);
+
+        List<Customer> findAll();
+
+    }
+    
+    public class Customer {
+
+        private final String name;
+
+        public Customer(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" + "name=" + name + '}';
+        }
+    }
+    
+    
+    
+    public class SimpleCustomerDao implements CustomerDao {
+
+        private Map<Integer, Customer> customers = new HashMap<>();
+
+        public SimpleCustomerDao(Map<Integer, Customer> customers) {
+            this.customers = customers;
+        }
+
+        @Override
+        public Optional<Customer> findById(int id) {
+            return Optional.ofNullable(customers.get(id));
+        }
+
+        @Override
+        public List<Customer> findAll() {
+            return new ArrayList<>(customers.values());
+        }
+    }
+    
+    public class CustomerService {
+
+        private final CustomerDao customerDao;
+
+        public CustomerService(CustomerDao customerDao) {
+            this.customerDao = customerDao;
+        }
+
+        public Optional<Customer> findById(int id) {
+            return customerDao.findById(id);
+        }
+
+        public List<Customer> findAll() {
+            return customerDao.findAll();
+        }
+    }
+    
+    /*
+    Referencia
+    https://github.com/eugenp/tutorials/tree/master/patterns-modules/dip/src/main/java/com/baeldung/dip
+    */
+ 
 }
+
