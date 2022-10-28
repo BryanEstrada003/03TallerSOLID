e484f99cc4f1b3b7b6df37ff23da413af372d842
Samsvill <samantha.sharid@gmail.com>
2022-10-27 21:55:19 -0500

    explicación de Notificacion

--- Ejemplos de SOLID/SOLID/src/main/java/seccionB/Notificacion.java
+++ Ejemplos de SOLID/SOLID/src/main/java/seccionB/Notificacion.java
@@ -1,7 +1,14 @@
 
 package seccionB;
 
+//OPEN-CLOSED PRINCIPLE: se viola el principio en el código
+//original ya que se debía agregar varios if cada que se quiera
+//agregar más opciones de notificaciones.
 
+//Se procedió a que los tipos de emails sean clases independientes
+//que implementen la interfaz Notificacion (el nombre se dejó por
+//legibilidad a comparación del código original, pero el nombre
+//correcto debe ser "Notificable".
 
 public interface Notificacion{
     public void notificar(Pagar pago);
